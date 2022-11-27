b924e0c619bce6b93e31e4b57d1a02c995304ccf
iamkervyntan <kervyntan@gmail.com>
2022-11-27 16:45:57 +0800

    traversing tree siblings

--- crypto-search.py
+++ crypto-search.py
@@ -5,4 +5,10 @@ url = "https://coinmarketcap.com"
 
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "html.parser")
-print(doc.tbody)
+
+tbody = doc.tbody
+content = tbody.contents # returns a list of all the tags inside the tbody
+
+# traversing the sibling tags (same level of tree)
+print(content[1].previousSibling)
+print(content[1].next_siblings)
