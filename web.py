d6ac50fb8afd42d8134cd85f7df266787f1927fa
iamkervyntan <kervyntan@gmail.com>
2022-11-27 15:39:20 +0800

    update web.py

--- web.py
+++ web.py
@@ -1,13 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 # print("Hello World")
-with open("index.html", "r") as f:
+with open("index2.html", "r") as f:
     # document passed in as f
     doc = BeautifulSoup(f, "html.parser")
     
 # access tags, use doc. the tag (gives the first instance)
 # print(doc.h1)
-
+#tag.attrs => gives you attributes in a dictionary
+res = doc.find('option')
+print(res)
 #access the html text
 # print(doc.h1.string)
 
@@ -22,9 +24,12 @@ tags = doc.find_all("p") #returns list
 
 url = "https://www.newegg.ca/gigabyte-geforce-rtx-3080-ti-gv-n308tgaming-oc-12gd/p/N82E16814932436?Description=3080&cm_re=3080-_-14-932-436-_-Product"
 
+"""
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "html.parser")
 prices = doc.find_all(text="$")
 parent = prices[0].parent
 strong = parent.find('strong')
 print(strong.string)
+
+"""
