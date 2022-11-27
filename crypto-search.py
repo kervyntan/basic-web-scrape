ca81ac9d07c9de089e0f6c6984b523f4035a318d
iamkervyntan <kervyntan@gmail.com>
2022-11-27 16:55:01 +0800

    loop through the list of items, store the data in prices dictionary

--- crypto-search.py
+++ crypto-search.py
@@ -10,5 +10,16 @@ tbody = doc.tbody
 content = tbody.contents # returns a list of all the tags inside the tbody
 
 # traversing the sibling tags (same level of tree)
-print(content[1].previousSibling)
-print(content[1].next_siblings)
+# print(content[1].previousSibling)
+# print(list(content[1].contents))
+
+prices = {}
+
+for tr in content[:10]:
+    name, price = tr.contents[2:4]
+    crypto_name = name.p.string
+    # print(price.a.string)
+    crypto_price = price.a.string
+    prices[crypto_name] = crypto_price
+    
+print(prices)
